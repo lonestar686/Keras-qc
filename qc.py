ddd9f63859a5b7d47e094a88ee7fce70446f83d3
hongbo zhou <lonestar686@gmail.com>
2018-01-11 11:23:19 -0600

    fixes some bugs and add something more.

--- qc.py
+++ qc.py
@@ -1,5 +1,12 @@
-# check information
+# check keras version/location
 print(keras.__version__, keras.__file__)
+# for tensorflow
 from tensorflow.python.client import device_lib
 print(device_lib.list_local_devices())
-print(tf.__version__)
+# tensorflow version/location
+import tensorflow as tf
+print(tf.__version__, tf.__file__)
+
+# remove redundant white spaces
+a = "bb   cc   dd"
+a = " ".join(a.split())
