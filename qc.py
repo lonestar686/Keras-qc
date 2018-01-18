77a0e2845718c0073afb7d00124997521a95da62
hongbo zhou <lonestar686@gmail.com>
2018-01-18 07:54:26 -0600

    Update qc.py

--- qc.py
+++ qc.py
@@ -1,11 +1,13 @@
 # check keras version/location
 print(keras.__version__, keras.__file__)
-# for tensorflow
-from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
+
 # tensorflow version/location
 import tensorflow as tf
 print(tf.__version__, tf.__file__)
+
+# check devices for tensorflow
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
 
 # remove redundant white spaces
 a = "bb   cc   dd"
