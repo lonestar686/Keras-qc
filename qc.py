830d671d0c508e245ab77a8a6ebf0a2e8f756423
hongbo zhou <lonestar686@gmail.com>
2018-04-01 13:28:42 -0500

    Update qc.py
    Add loading tf model from keras

--- qc.py
+++ qc.py
@@ -12,3 +12,24 @@ print(device_lib.list_local_devices())
 # remove redundant white spaces
 a = "bb   cc   dd"
 a = " ".join(a.split())
+
+# https://stackoverflow.com/questions/44466066/how-can-i-convert-a-trained-tensorflow-model-to-keras
+# The ckpt file can be saved by TF with:
+saver = tf.train.Saver()
+saver.save(sess, checkpoint_name)
+
+#and to load checkpoint in Keras, you need a callback class as follow:
+class MyCallbacks(keras.callbacks.Callback):
+    def __init__(self, pretrained_file):
+        self.pretrained_file = pretrained_file
+        self.sess = keras.backend.get_session()
+        self.saver = tf.train.Saver()
+    def on_train_begin(self, logs=None):
+        if self.pretrian_model_path:
+            self.saver.restore(self.sess, self.pretrian_model_path)
+            print('load weights: OK.')
+
+ #Then in your keras script:
+ model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+ testCallBack = MyCallbacks(pretrian_model_path='./XXXX.ckpt') 
+ model.fit(x_train, y_train, batch_size=128, epochs=20, callbacks=[testCallBack])
